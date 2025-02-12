a47103d327606565f48d1b5a110d2cf9b394f9c4
Leo <punzalank03@gmail.com>
2025-02-12 10:55:20 -0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- 05_post_check.sh
+++ 05_post_check.sh
@@ -1,4 +1,4 @@
-
+	
 # Expected table structure (adjust as needed)
 EXPECTED_STRUCTURE=$(cat# Get the actual table structure
 actual_structure=$(mysql -h 127.0.0.1 -P 4000 -u root UniversityDB -e "DESCRIBE>
@@ -6,8 +6,8 @@ actual_structure=$(mysql -h 127.0.0.1 -P 4000 -u root UniversityDB -e "DESCRIBE>
 # Format the expected structure for comparison (remove extra whitespace)
 expected_formatted=$(echo "$EXPECTED_STRUCTURE" | sed 's/^[ \t]*//; s/[ \t]*$//>
 
-# Format the actual structure for comparison (remove extra whitespace)
-actual_formatted=$(echo "$actual_structure" | sed 's/^[ \t]*//; s/[ \t]*$//; s/>
+# Format the actual_formatted=$(echo "$actual_structure" | sed 's/^[ \t]*//; s/[ \t]*$//; s/>//g')
+
 
 
 # Compare the structures
